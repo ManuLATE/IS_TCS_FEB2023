68ce7a11469c5897d98914c4ada670c7b6789eb5
ManuLATE <120355376+ManuLATE@users.noreply.github.com>
2023-04-27 11:26:38 -0600

    Add files via upload

---
+++ ServiciosConvertidor/Convertidor/ServicioMoneda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Convertidor
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class ServicioMoneda : IServicioMoneda
+    {
+        public double convertirDolares(double dolares)
+        {
+            double pesos = dolares * 18.16;
+            return pesos;
+        }
+
+        public double convertirPesos(double pesos)
+        {
+            double dolares = pesos / 18.16;
+            return dolares;
+        }
+    }
+}
